bb5c2f4d0ecae1171343e907607daecc398a380c
Robert Lyons <rlyons@apm.com>
2017-10-26 12:10:14 -0700

    added kernel patch step to stepper

--- server/controllers/query.js
+++ server/controllers/query.js
@@ -67,11 +67,12 @@ function execAsyncQuery(pool, query) {
 module.exports = {
 
   getLabData (req, res) {
-    let pool = initConnectionPool(3)
+    let pool = initConnectionPool(4)
     var promises = [
       // get users
       execAsyncQuery(pool, 'SELECT user_name as username, display_name as name from tg_user \
-                            WHERE user_name NOT LIKE \'beaker-controller-rack%\''),
+                            WHERE user_name NOT LIKE \'beaker-controller-rack%\' \
+                            AND removed IS NULL'),
       // get distros and distro families (present on a lab controller)
       execAsyncQuery(pool, 'SELECT DISTINCT OS.osmajor, D.name as distro from \
                             distro D join distro_tree DT on D.id=DT.distro_id \
@@ -80,6 +81,8 @@ module.exports = {
                             join osmajor OS on O.osmajor_id=OS.id'),
       // get available system pools
       execAsyncQuery(pool, 'SELECT name FROM system_pool'),
+      // get all tasks that can be executed
+      execAsyncQuery(pool, 'SELECT name FROM task'),
     ]
     return Promise.all(promises)
       .then(queryData => {
@@ -103,25 +106,41 @@ module.exports = {
         for (let prop in distros_temp) {
           distros.push(distros_temp[prop])
         }
+        //format task data
+        let tests_temp = {}
+        let tests = []
+        for (let i of queryData[3]) {
+          let cat = i.name.split('/')[1]
+          if (!tests_temp[cat]) tests_temp[cat] = {category: cat, tasks: [i.name]}
+          else tests_temp[cat].tasks.push(i.name)
+        }
+        for (let prop in tests_temp) {
+          tests.push(tests_temp[prop])
+        }
+        tests.sort(function(t1, t2) {
+          var A = t1.category.toLowerCase();
+          var B = t2.category.toLowerCase();
+          return (A < B) ? -1 : (A > B) ? 1 : 0;
+        })
         // get the APM linux versions on the local drive
         fs.readdir('/home/amcclab/Documents/job_submission/static', (err, files) => {
           if (err) {
             console.log(err)
             throw new Error('FS READDIR ERROR')
           }
-          let versions = []
+          let patches = []
           files.forEach(file => {
-            versions.push(file)
+            patches.push({
+              name: file
+            })
           });
-          distros.push({
-            name: 'APM CentOS',
-            versions: versions
-          })
           // compile resulting data object
           let result = {
             users: queryData[0],
             distros: distros,
-            pools: queryData[2]
+            pools: queryData[2],
+            tests: tests,
+            patches: patches
           }
           // allow CORS for speedy development
           res.set({
